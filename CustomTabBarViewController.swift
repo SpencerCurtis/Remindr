46bfa08c363c558f7794f3f2a24a4faa9f933874
Spencer Curtis <spencercolecurtis@gmail.com>
2017-02-07 14:01:56 -0700

    Custom Tab Bar Controller UI design (Version 1)
    complete.

--- CustomTabBarViewController.swift
+++ CustomTabBarViewController.swift
@@ -8,34 +8,54 @@
 
 import UIKit
 
-class CustomTabBarViewController: UITabBarController, CustomTabBarDataSource {
+class CustomTabBarViewController: UITabBarController, CustomTabBarDelegate {
     
+    var customTabBar: CustomTabBar!
+    
+    override var selectedIndex: Int {
+        didSet {
+            customTabBar.selectIndex(index: selectedIndex)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBar.isHidden = true
+
+        self.tabBar.backgroundColor = .clear
+        customTabBar = CustomTabBar()
+        customTabBar.delegate = self
+        customTabBar.selectIndex(index: 0)
         
-        
-        
-        let customTabBar = CustomTabBar(frame: self.tabBar.frame)
         self.view.addSubview(customTabBar)
+        constraintsFor(customTabBar: customTabBar)
         
-        customTabBar.dataSource = self
+    }
+    
+    func constraintsFor(customTabBar: CustomTabBar) {
+        customTabBar.translatesAutoresizingMaskIntoConstraints = false
         
+        let leadingConstraint = NSLayoutConstraint(item: customTabBar, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: customTabBar, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: customTabBar, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: customTabBar, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 1/9, constant: 0)
         
+        self.view.addConstraints([leadingConstraint, trailingConstraint, bottomConstraint, heightConstraint])
+        
+    }
+    
+    func tabBarButtonTapped(index: Int) {
+        selectedIndex = index
+    }
+    
+    func segue(notNeededMaybe: String) {
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tabBarItemsInCustomTabBar(tabBarView: CustomTabBar) -> [UITabBarItem] {
-        guard let items = tabBar.items else { return [] }
-        
-        return items
     }
     /*
      // MARK: - Navigation
---
+++ Remindr/CustomTabBarView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTabBarView.swift
+//  Remindr
+//
+//  Created by Spencer Curtis on 1/31/17.
+//  Copyright © 2017 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
